cf0b42cf8551a543a8fda37726cc5276ffc1d45b
Sinan <sinanmhaque@gmail.com>
2025-10-15 23:48:56 +1100

    Done till 8.5

--- test/Interpreter.java
+++ test/Interpreter.java
@@ -3,9 +3,13 @@ package test;
 import java.util.List;
 
 import test.*;
+import test.Expr.Variable;
+import test.Stmt.Var;
 
 class Interpreter implements Expr.Visitor<Object>,
         Stmt.Visitor<Void> {
+
+    private Environment environment = new Environment();
 
     void interpret(List<Stmt> statements) {
         try {
@@ -161,5 +165,32 @@ class Interpreter implements Expr.Visitor<Object>,
 
         return object.toString();
     }
+
+    @Override
+    public Void visitVarStmt(Stmt.Var stmt) {
+        Object value = null;
+        if (stmt.initializer != null) {
+            value = evaluate(stmt.initializer);
+        }
+
+        environment.define(stmt.name.lexeme, value);
+        return null;
+    }
+
+    @Override
+    public Object visitVariableExpr(Expr.Variable expr) {
+        return environment.get(expr.name);
+    }
+
+    
+
+    @Override
+    public Object visitAssignExpr(Expr.Assign expr) {
+        Object value = evaluate(expr.value);
+        environment.assign(expr.name, value);
+        return value;
+    }
+
+    
 
 }
